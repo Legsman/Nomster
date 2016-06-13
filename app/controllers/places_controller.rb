20769d95370e9b07f0057f5864decbeae3d0e94d
Antoine <maciaantoine@yahoo.fr>
2016-06-13 13:06:04 +0000

    Added extra validations to create place form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,14 +9,14 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
-  def create
-    current_user.places.create(place_params)
-    if @place.valid?
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
+ def create
+  @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
   def show
     @place = Place.find(params[:id])
