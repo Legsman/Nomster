c0e55fc603611c2e96fc361f8ea8caff2d47a3e5
Antoine <maciaantoine@yahoo.fr>
2016-06-17 04:50:34 +0000

    Added URL to email sent

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@koffee.com"
 
-  def comment_added
-    mail(to: "antoine.macia83@gmail.com",
-    subject: "a comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+    subject: "a comment has been added to #{@place.name}")
   end
 end
